7f1d6fca3186455dc7f340c4fac3555bf7ad145b
Elliot Gagola <egagola@spartaglobal.com>
2018-10-25 10:16:56 +0100

    Added dynamic titles

--- controllers/post_controller.rb
+++ controllers/post_controller.rb
@@ -14,19 +14,21 @@ class PostController < Sinatra::Base
   end
 
   get "/:id" do
-    id = params[:id]
-
+    id = params[:id].downcase
+    @title = id.capitalize
     case id
-    when "england"
-      erb :'posts/england'
-    when "germany"
-      erb :'posts/germany'
-    when "mexico"
-      erb :'posts/mexico'
-    when "china"
-      erb :'posts/china'
-    when "australia"
-      erb :'posts/australia'
-    end
+      when "england"
+        erb :'posts/england'
+      when "germany"
+        erb :'posts/germany'
+      when "mexico"
+        erb :'posts/mexico'
+      when "china"
+        erb :'posts/china'
+      when "australia"
+        erb :'posts/australia'
+      else
+        redirect :"https://en.wikipedia.org/wiki/#{id}"
+      end
   end
 end
